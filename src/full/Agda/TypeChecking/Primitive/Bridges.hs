53ab771b5223eecce2bcb88147ea8d1bfffb21bf
Antoine Van Muylder <antvanmul@hotmail.com>
2021-10-19 16:32:36 +0200

    [bridges] Wip. Write type of extent in internal syntax

--- src/full/Agda/TypeChecking/Primitive/Bridges.hs
+++ src/full/Agda/TypeChecking/Primitive/Bridges.hs
@@ -66,3 +66,55 @@ requireBridges s = do
 -- as it is the case for tick variables.
 primBridgeIntervalType :: (HasBuiltins m, MonadError TCErr m, MonadTCEnv m, ReadTCState m) => m Type
 primBridgeIntervalType = El LockUniv <$> primBridgeInterval
+
+
+
+-- | type for extent primitive.
+-- We use the hoas style functions like hPi' (hidden pi) to specifiy types in internal syntax.
+-- primExtent : ∀ {ℓA ℓB} {A : BI → Set ℓA} {B : (x : BI) (a : A x) → Set ℓB}
+--              (r : BI) (M : A r)
+--              (N0 : (a0 : A bi0) → B bi0 a0)
+--              (N1 : (a1 : A bi1) → B bi1 a1)
+--              (NN : (a0 : A bi0) (a1 : A bi1) (aa : BridgeP A a0 a1) → BridgeP (λ x → B x (aa x)) (N0 a0) (N1 a1)) →
+--              BridgeP (λ x → (a : A x) → B x a) N0 N1
+extentType :: TCM Type
+extentType = do
+  requireBridges "in extentType"
+  t <- runNamesT [] $
+       hPi' "lA" (el $ cl primLevel) (\ lA ->
+       hPi' "lB" (el $ cl primLevel) $ \ lB ->
+       hPi' "A" (primBridgeIntervalType --> (sort . tmSort <$> lA)) $ \ bA ->
+       hPi' "B" (nPi' "x" primBridgeIntervalType $ \ x -> (el' lA (bA <@> x)) --> (sort . tmSort <$> lB) ) $ \bB ->
+       nPi' "r" primBridgeIntervalType $ \ r ->
+       -- below A r is an IApply elim instead of std argument, might come in handy?
+       nPi' "M" (el' lA (bA <@@> (bA <@> cl primBIZero, bA <@> cl primBIOne, r) )) $ \bM ->
+       nPi' "N0" (nPi' "a0" (el' lA (bA <@> cl primBIZero)) $ \a0 -> (el' lB (bB <@> cl primBIZero <@> a0))) $ \n0 ->
+       nPi' "N1" (nPi' "a1" (el' lA (bA <@> cl primBIOne)) $ \a1 -> (el' lB (bB <@> cl primBIOne <@> a1))) $ \n1 ->
+       nPi' "NN"
+        (nPi' "a0" (el' lA (bA <@> cl primBIZero)) $ \a0 ->
+         nPi' "a1" (el' lA (bA <@> cl primBIOne)) $ \a1 ->
+         primBridgeIntervalType) $ \nn ->
+       primBridgeIntervalType)
+  return t
+                                         
+-- prim_glue' :: TCM PrimitiveImpl
+-- prim_glue' = do
+--   requireCubical CFull ""
+--   t <- runNamesT [] $
+--        hPi' "la" (el $ cl primLevel) (\ la ->
+--        hPi' "lb" (el $ cl primLevel) $ \ lb ->
+--        hPi' "A" (sort . tmSort <$> la) $ \ a ->
+--        hPi' "φ" primIntervalType $ \ φ ->
+--        hPi' "T" (pPi' "o" φ $ \ o ->  el' (cl primLevelSuc <@> lb) (Sort . tmSort <$> lb)) $ \ t ->
+--        hPi' "e" (pPi' "o" φ $ \ o -> el' (cl primLevelMax <@> la <@> lb) $ cl primEquiv <#> lb <#> la <@> (t <@> o) <@> a) $ \ e ->
+--        pPi' "o" φ (\ o -> el' lb (t <@> o)) --> (el' la a --> el' lb (cl primGlue <#> la <#> lb <@> a <#> φ <@> t <@> e)))
+--   view <- intervalView'
+--   one <- primItIsOne
+--   return $ PrimImpl t $ primFun __IMPOSSIBLE__ 8 $ \ts ->
+--     case ts of
+--       [la,lb,bA,phi,bT,e,t,a] -> do
+--        sphi <- reduceB' phi
+--        case view $ unArg $ ignoreBlocking $ sphi of
+--          IOne -> redReturn $ unArg t `apply` [argN one]
+--          _    -> return (NoReduction $ map notReduced [la,lb,bA] ++ [reduced sphi] ++ map notReduced [bT,e,t,a])
+--       _ -> __IMPOSSIBLE__
